1689fa815eef461686cc62a9867966d57e49db68
rukayaj@gmail.com <rukayaj@gmail.com>
2019-03-08 15:57:03 +0100

    Add app with skeleton views

--- uuidresolver/settings.py
+++ uuidresolver/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    'website.apps.WebsiteConfig',
     'django.contrib.staticfiles',
 ]
 
--- uuidresolver/urls.py
+++ uuidresolver/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from website import views
 
 urlpatterns = [
+        path('', views.index, name='index'),
+        path('<uuid:uuid>/', views.detail, name='detail'),
 ]
