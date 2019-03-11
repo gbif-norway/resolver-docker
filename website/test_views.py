1ac6dd874fd09b01644cc8473c8cf90f4b633fdf
rukayaj@gmail.com <rukayaj@gmail.com>
2019-03-11 09:56:18 +0100

    Add detail view and tests

---
+++ website/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase #, Client
+#from django.test.utils import setup_test_environment
+from .models import DarwinCoreObject
+from django.urls import reverse
+
+class ResolverViewTests(TestCase):
+
+    def test_displays_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_displays_data_given_valid_uuid(self):
+        uuid = '5c0884ce-608c-4716-ba0e-cb389dca5580'
+        obj_1 = DarwinCoreObject.objects.create(uuid=uuid, data={'id': uuid, 'type': 'occurrence'})
+        response = self.client.get(reverse('detail', args=[uuid]))
+        response_string = response.content.decode('utf-8').lower()
+        self.assertTrue('occurrence' in response_string)
+        self.assertTrue(uuid in response_string)
--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import DarwinCoreObject
 
 def index(request):
     return HttpResponse('Resolver')
 
 def detail(request, uuid):
-    return HttpResponse('Details')
+    return render(request, 'detail.html', {'darwin_core_object': DarwinCoreObject.objects.get(uuid=uuid)})
