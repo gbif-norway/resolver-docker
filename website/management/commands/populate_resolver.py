d3f81b3c95d7f3a611f491619585f94ba07e47a7
rukayaj@gmail.com <rukayaj@gmail.com>
2019-04-01 16:56:06 +0200

    Add tests for emailing errors

--- website/management/commands/populate_resolver.py
+++ website/management/commands/populate_resolver.py
@@ -3,31 +3,37 @@ from django.core import mail
 from website.models import DarwinCoreObject
 from website.management.commands import _gbif_api
 from website.management.commands import _darwin_core_processing
+from django.db import connection
+import datetime
 
 class Command(BaseCommand):
     help = 'Populates the resolver from datasets added to GBIF by Norwegian IPTs'
 
     def handle(self, *args, **options):
         total_added = 0
-        _create_replacement_table()
+        self._create_replacement_table()
         for dataset in _gbif_api.get_dataset_list():
             endpoints = _gbif_api.get_dataset_endpoints(dataset['key'])
             darwin_core_endpoint = _gbif_api.get_dwc_endpoint(endpoints)
 
-            with _gbif_api.get_cores_from_ipt(darwin_core_endpoint['url']) as cores:
-                for core_type, file_obj in cores:
-                    core_id_key = _darwin_core_processing.get_core_id(core_type)
-                    if core_id_key:
-                        darwin_core_objects = _darwin_core_processing.copy_csv_to_replacement_table(file_obj, core_id_key)
-                        total_added += len(darwin_core_objects)
+            cores = _gbif_api.get_cores_from_ipt(darwin_core_endpoint['url'])
+            for core_type, file_obj in cores:
+                core_id_key = _darwin_core_processing.get_core_id(core_type)
+                if core_id_key:
+                    count_of_added = _darwin_core_processing.copy_csv_to_replacement_table(file_obj, core_id_key)
+                    if count_of_added:
+                        total_added += count_of_added
                     else:
-                        _email_error('Core ID not supported %s' % (core_type), 'File : %s' % (darwin_core_endpoint['url']))
+                        self._email_error('No items added for %s' % (core_type), 'File : %s' % (darwin_core_endpoints['url']))
+                else:
+                    self._email_error('Core type not supported: %s' % (core_type), 'File url: %s' % (darwin_core_endpoint['url']))
 
-    def _email_error(e, subject):
-        exc_info = sys.exc_info()
-        mail.mail_admins(subject, '\n'.join(traceback.format_exception(*exc_info)), fail_silently=True)
+        self._email_error('Resolver import complete %s' % datetime.datetime.now().strftime("%Y-%m-%d"), 'Total number of rows imported %s' % total_added)
 
-    def _create_replacement_table():
+    def _email_error(self, subject, message):
+        mail.mail_admins(subject, message, fail_silently=True)
+
+    def _create_replacement_table(self):
         with connection.cursor() as cursor:
             create_table_sql = 'CREATE TABLE replacement_table (uuid uuid, data jsonb)'
             cursor.execute(create_table_sql)
--- website/tests/test_management_commands.py
+++ website/tests/test_management_commands.py
@@ -2,13 +2,49 @@ from io import StringIO
 from django.core.management import call_command
 from django.test import TestCase
 import responses
-import requests
+from unittest import mock
+from website.management.commands import _gbif_api
+from django.core import mail
+from io import StringIO
+from django.db import connection
+import datetime
 
 class PopulateResolverTest(TestCase):
+    GBIF_API_DATASET_URL = "https://api.gbif.org/v1/dataset/{}"
+    endpoints_example = [{'type': 'DWC_ARCHIVE', 'url': 'http://data.gbif.no/archive.do?r=dataset'}]
 
-    def test_command_output(self):
-        out = StringIO()
-        #call_command('populate_resolver', stdout=out)
-        #self.assertIn('test', out.getvalue())
+    @responses.activate
+    def test_bad_core_email_alert(self):
+        self._mock_get_dataset_list()
+        self._mock_get_dataset_endpoints()
+        with mock.patch('website.management.commands._gbif_api.get_cores_from_ipt', return_value=[('incorrect_core_type', StringIO('file_obj'))]):
+            call_command('populate_resolver', stdout=StringIO())
+        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(mail.outbox[0].subject, "[Django] Core type not supported: incorrect_core_type")
+        self.assertIn(mail.outbox[0].body, "File url: http://data.gbif.no/archive.do?r=dataset")
+        self.assertEqual(mail.outbox[1].subject, '[Django] Resolver import complete %s' % datetime.datetime.now().strftime("%Y-%m-%d"))
+        self.assertIn(mail.outbox[1].body, 'Total number of rows imported 0')
 
+    @responses.activate
+    def test_bad_core_still_adds_other_valid_cores(self):
+        self._mock_get_dataset_list()
+        self._mock_get_dataset_endpoints()
+        with open('website/tests/occurrence_test_file_small.txt') as file_obj:
+            cores = [('incorrect_core_type', StringIO('file_obj')), ('occurrence', file_obj)]
+            with mock.patch('website.management.commands._gbif_api.get_cores_from_ipt', return_value=cores):
+                call_command('populate_resolver', stdout=StringIO())
+
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM replacement_table")
+            self.assertEqual(cursor.fetchone()[0], 5000)
+
+    def _mock_get_dataset_list(self):
+        mock_datasets = [{'key': 'd34ed8a4-d3cb-473c-a11c-79c5fec4d649'}]
+        mock_json = {'offset': 0, 'limit': 200, 'endOfRecords': 1, 'count': 200, 'results': mock_datasets}
+        api_url = self.GBIF_API_DATASET_URL.format('search?limit=5000&publishingCountry=NO')
+        responses.add(responses.GET, api_url, json=mock_json, status=200)
+
+    def _mock_get_dataset_endpoints(self):
+        url = self.GBIF_API_DATASET_URL.format('d34ed8a4-d3cb-473c-a11c-79c5fec4d649')
+        responses.add(responses.GET, url, json={'key':'d34ed8a4-d3cb-473c-a11c-79c5fec4d649', 'endpoints': self.endpoints_example}, status=200)
 
