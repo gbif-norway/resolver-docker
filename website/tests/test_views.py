b12cfa5f44c9a5afc7f46bc83253b4ac89561927
rukayaj@gmail.com <rukayaj@gmail.com>
2019-04-03 16:28:30 +0200

    Copy views from old resolver

--- website/tests/test_views.py
+++ website/tests/test_views.py
@@ -9,13 +9,32 @@ class ResolverViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_displays_data_given_valid_uuid(self):
-        uuid = '5c0884ce-608c-4716-ba0e-cb389dca5580'
-        obj_1 = DarwinCoreObject.objects.create(uuid=uuid, data={'id': uuid, 'type': 'occurrence'})
-        response = self.client.get(reverse('detail', args=[uuid]))
-        response_string = response.content.decode('utf-8').lower()
+        response_string = self._simple_request('text/html')
         self.assertTrue('occurrence' in response_string)
-        self.assertTrue(uuid in response_string)
+        self.assertTrue('5c0884ce-608c-4716-ba0e-cb389dca5580' in response_string)
 
     def test_displays_404_given_invalid_uuid(self):
         response = self.client.get(reverse('detail', args=['00000000-0000-0000-0000-000000000000']))
         self.assertContains(response, 'not found', status_code=404)
+
+    def test_renders_json(self):
+        response_string = self._simple_request('application/json')
+        self.assertTrue('occurrence' in response_string)
+        self.assertTrue('5c0884ce-608c-4716-ba0e-cb389dca5580' in response_string)
+
+    def test_renders_n3(self):
+        response_string = self._simple_request('text/n3')
+        self.assertTrue('occurrence' in response_string)
+        self.assertTrue('5c0884ce-608c-4716-ba0e-cb389dca5580' in response_string)
+
+    def test_renders_xml(self):
+        response_string = self._simple_request('application/xml')
+        self.assertTrue('occurrence' in response_string)
+        self.assertTrue('5c0884ce-608c-4716-ba0e-cb389dca5580' in response_string)
+
+    def _simple_request(self, http_accept):
+        uuid = '5c0884ce-608c-4716-ba0e-cb389dca5580'
+        obj_1 = DarwinCoreObject.objects.create(uuid=uuid, data={'uuid': uuid, 'type': 'occurrence'})
+        response = self.client.get(reverse('detail', args=[uuid]), HTTP_ACCEPT=http_accept)
+        return response.content.decode('utf-8').lower()
+
--- website/views.py
+++ website/views.py
@@ -1,10 +1,56 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import DarwinCoreObject
+from django.core import serializers
+from rdflib import Graph
+from django.forms.models import model_to_dict
+import json
 
 def index(request):
     return HttpResponse('Resolver')
 
 def detail(request, uuid):
     darwin_core_object = get_object_or_404(DarwinCoreObject, uuid=uuid)
+
+    mime_types = {
+     '*/*': _html(request, darwin_core_object),
+     'text/html': _html(request, darwin_core_object),
+     'application/json': JsonResponse(_json(darwin_core_object)),
+     'application.ld+json': JsonResponse(_json(darwin_core_object)),
+     'text/plain': _text(request, darwin_core_object),
+     'text/n3': _n3(darwin_core_object),
+     'text/turtle': _n3(darwin_core_object),
+     'text/xml': _rdf(darwin_core_object),
+     'application/xml': _rdf(darwin_core_object),
+     'application/rdf+xml': _rdf(darwin_core_object)
+    }
+
+    content_type = request.META.get("HTTP_ACCEPT")
+    if content_type in mime_types:
+        return mime_types[content_type]
+    else:
+        return HttpResponse('Unrecognised MIME type in request headers')
+
+def _html(request, darwin_core_object):
     return render(request, 'detail.html', {'darwin_core_object': darwin_core_object})
+
+def _text(request, darwin_core_object):
+    return render(request, 'detail.html', {'darwin_core_object': darwin_core_object}, content_type='text/plain')
+
+def _json(darwin_core_object):
+    json_object = model_to_dict(darwin_core_object)['data']
+    prefixed_object = {'dwc:%s' % key: value for key, value in json_object.items()}
+    prefixed_object['@id'] = 'http://purl.org/gbifnorway/id/%s' % darwin_core_object.uuid
+    prefixed_object['@context'] = {'dc': 'http://purl.org/dc/elements/1.1/', 'dwc': 'http://rs.tdwg.org/dwc/terms/'}
+    return prefixed_object
+
+def graph(darwin_core_object):
+    json_object = _json(darwin_core_object)
+    return Graph().parse(data=json.dumps(json_object), format='json-ld')
+
+def _n3(darwin_core_object):
+    return graph(darwin_core_object).serialize(format='n3')
+
+def _rdf(darwin_core_object):
+    return HttpResponse(graph(darwin_core_object).serialize(), content_type='application/rdf+xml')
+
