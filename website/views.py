f4d5e0633b5f31b9b55961acef0ec0656bf84f7c
rukayaj@gmail.com <rukayaj@gmail.com>
2019-07-01 15:13:04 +0200

    Add filter

--- website/views.py
+++ website/views.py
@@ -2,13 +2,15 @@ from .models import DarwinCoreObject
 from rest_framework import viewsets, renderers
 from .serializers import DarwinCoreObjectSerializer
 from .renderers import RDFRenderer, JSONLDRenderer
+from rest_framework import filters
 
 
 class DarwinCoreObjectViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    A ViewSet for reading Darwin Core Objects
+    GBIF Norway's resolver provides darwin core object data from datasets published to gbif.org by Norwegian publishers
     """
     renderer_classes = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer, JSONLDRenderer, RDFRenderer)
     queryset = DarwinCoreObject.objects.all()
     serializer_class = DarwinCoreObjectSerializer
-
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('data', )
