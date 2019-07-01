074915990b9351f1deff30f1d85d6b97ab559ff7
rukayaj@gmail.com <rukayaj@gmail.com>
2019-07-01 10:54:27 +0200

    Simplify urls

--- uuidresolver/urls.py
+++ uuidresolver/urls.py
@@ -17,8 +17,8 @@ from django.urls import include, path
 from website import views
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'dwc-objects', views.DarwinCoreObjectViewSet)
+router = routers.SimpleRouter()
+router.register(r'', views.DarwinCoreObjectViewSet)
 
 urlpatterns = [
         path('', include(router.urls)),
