7c00aa0a867293bcfb46d3c9ea9844cb80a6c7bc
rukayaj@gmail.com <rukayaj@gmail.com>
2020-02-26 13:29:41 +0100

    Fix pagination

--- website/views.py
+++ website/views.py
@@ -8,7 +8,7 @@ from django.db.models import Q
 
 class DarwinCoreObjectViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    GBIF Norway's resolver provides darwin core object data from datasets published to gbif.org by Norwegian publishers
+    GBIF Norway's resolver provides data published to gbif.org by Norwegian publishers. Query by appending e.g. `?data__type=dataset` to get a list of all datasets, or `?data__scientificname=Galium+odoratum` to filter on scientific name.
     """
     renderer_classes = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer, JSONLDRenderer, RDFRenderer)
     queryset = DarwinCoreObject.objects.all()
@@ -18,6 +18,7 @@ class DarwinCoreObjectViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         q_objects = Q()
         for field_name, search_term in self.request.query_params.items():
-            q_objects &= Q(**{field_name:search_term})
+            if field_name[:6] == 'data__':
+                q_objects &= Q(**{field_name:search_term})
         return DarwinCoreObject.objects.filter(q_objects)
 
