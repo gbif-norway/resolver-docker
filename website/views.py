5aa83b525126204d0cd755f303ebd7df401c9765
rukayaj <rukayaj@gmail.com>
2020-06-24 10:25:59 +0200

    Added CREATE INDEX idxginscientificname ON website_darwincoreobject USING GIN ((data -> 'scientificname')); use index

--- website/views.py
+++ website/views.py
@@ -8,7 +8,7 @@ from django.db.models import Q
 
 class DarwinCoreObjectViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    GBIF Norway's resolver provides data published to gbif.org by Norwegian publishers. Query by appending e.g. `?data__type=dataset` to get a list of all datasets, or `?data__scientificname=Galium+odoratum` to filter on scientific name.
+    GBIF Norway's resolver provides data published to gbif.org by Norwegian publishers. Query by appending e.g. `?type=dataset` to get a list of all datasets, or `?scientificname=Galium+odoratum` to filter on scientific name.
     """
     renderer_classes = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer, JSONLDRenderer, RDFRenderer)
     queryset = DarwinCoreObject.objects.all()
@@ -16,9 +16,5 @@ class DarwinCoreObjectViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = CustomPagination
 
     def get_queryset(self):
-        q_objects = Q()
-        for field_name, search_term in self.request.query_params.items():
-            if field_name[:6] == 'data__':
-                q_objects &= Q(**{field_name:search_term})
-        return DarwinCoreObject.objects.filter(q_objects)
+        return DarwinCoreObject.objects.filter(data__contains=self.request.query_params)
 
