04a4e6575709ce2302814e5600fdb4a062759dc8
rukayaj@gmail.com <rukayaj@gmail.com>
2020-07-16 11:10:33 +0200

    Allow historical caching

--- website/models.py
+++ website/models.py
@@ -5,3 +5,9 @@ import uuid
 class DarwinCoreObject(models.Model):
     id = models.CharField(primary_key=True, max_length=200)
     data = JSONField()
+    deleted_date = models.DateField(null=True, blank=True)
+
+class History(models.Model):
+    darwin_core_object = models.ForeignKey(DarwinCoreObject, on_delete=models.CASCADE)
+    changed_data = JSONField()
+    changed_date = models.DateField()
