4cb594fe5302e7b2320af26a4948fabd8febdcc7
rukayaj@gmail.com <rukayaj@gmail.com>
2020-07-23 09:51:24 +0200

    Add history table

--- website/models.py
+++ website/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.indexes import GinIndex
 import uuid
 
 class DarwinCoreObject(models.Model):
     id = models.CharField(primary_key=True, max_length=200)
     data = JSONField()
     deleted_date = models.DateField(null=True, blank=True)
+    created_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        indexes = [GinIndex(fields=['data'])]
+        # CREATE INDEX idxginscientificname ON website_darwincoreobject USING GIN ((data -> 'scientificname'));
+
 
 class History(models.Model):
-    darwin_core_object = models.ForeignKey(DarwinCoreObject, on_delete=models.CASCADE)
+    darwin_core_object = models.ForeignKey(DarwinCoreObject, on_delete=models.DO_NOTHING)
     changed_data = JSONField()
     changed_date = models.DateField()
