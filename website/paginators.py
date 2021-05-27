3dc9fd49720641ac2bd0f788a215870be819be90
rukayaj@gmail.com <rukayaj@gmail.com>
2021-05-27 14:10:05 +0200

    Exclude count from pagination

--- website/paginators.py
+++ website/paginators.py
@@ -1,5 +1,7 @@
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from populator.models import Statistic
+from collections import OrderedDict
 
 
 class CustomPagination(LimitOffsetPagination):
@@ -10,5 +12,12 @@ class CustomPagination(LimitOffsetPagination):
             return Statistic.objects.get_total_count()
         elif 'WHERE "website_resolvableobject"."data" @> {"basisofrecord": "Preservedspecimen"}' in str(queryset.query):
             return Statistic.objects.get_preserved_specimen_count()
-        return queryset.count()
+        return 500
+        #return queryset.count()
 
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
