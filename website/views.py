4554bc740dd890e0879c6c88d243bae8fe1bd035
rukayaj@gmail.com <rukayaj@gmail.com>
2022-08-24 16:47:14 +0200

    Allow urn:uuid: prefix in url

--- website/views.py
+++ website/views.py
@@ -7,6 +7,7 @@ from .paginators import CustomPagination, CustomCountPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from collections import defaultdict
 import json
+from django.shortcuts import get_object_or_404
 
 
 class HistoryViewSet(viewsets.ReadOnlyModelViewSet):
@@ -64,6 +65,27 @@ class ResolvableObjectViewSet(viewsets.ReadOnlyModelViewSet):
             self.pagination_class = CustomCountPagination
         return ResolvableObject.objects.filter(**args)
 
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        # Remove urn:uuid:prefix
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg].replace('urn:uuid:', '')}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
 class DatasetViewSet(viewsets.ReadOnlyModelViewSet):
     """
