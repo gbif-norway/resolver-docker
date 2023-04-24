ce8ee076ff7086ac372b971374a04de62a06f1e6
rukayaj@gmail.com <rukayaj@gmail.com>
2023-04-24 15:10:43 +0200

    Port 8080

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ python manage.py migrate
 
 # Start the server
 # exec python manage.py runserver 0.0.0.0:8080
-exec gunicorn resolver.wsgi:application --bind 0.0.0.0:80 --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread
+exec gunicorn resolver.wsgi:application --bind 0.0.0.0:8080 --worker-tmp-dir /dev/shm --workers=2 --threads=4 --worker-class=gthread
 
